91c04d79e74c68c4546bfdef7eef9b7d11856679
Moustafa El Dehni <moustafa.eldehni@hotmail.com>
2025-05-13 21:16:30 +0200

    test deploy 5

--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -10,8 +10,8 @@ export default function Section() {
                 <span>Plus récents<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg></span>
             </div>
             <div className='allGames'>
-                {data.map(element => (   
-                    <div className='game' key={element.title}>
+                {data.map((element, index) => (   
+                    <div className='game' key={index}>
                         <div className='gamePicture'>
                             <img className='gamePictureIn' src={element.image} alt={element.title} />
                         </div>
