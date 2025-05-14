169115a4754f6f26edf289a6941b9f0324094371
chozen <moustafa.eldehni@hotmail.com>
2025-05-14 14:57:23 +0200

    filtrage done

--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -1,12 +1,27 @@
 import './section.css'
 import data from "./data.json"
+import { useEffect, useState } from 'react'
 
 export default function Section({categorieFiltre}) {
+    
+    const [animate, setAnimate] = useState(false)
+    
+    useEffect(()=>{
+        setAnimate(false)
+        const timeout = setTimeout(()=>{
+            setAnimate(true)
+        }, 10)
+        const timezout = setTimeout(()=>{
+            setAnimate(false)
+        }, 300)
+        return() => clearTimeout(timezout)
+    }, [categorieFiltre])
+
     return(
         <section className='section'>
             <div className='allGames'>
-                {data.map((element, index) => (   
-                    <div className='game' key={index}>
+                {categorieFiltre.map(element => (   
+                    <div className={`game ${animate === true ? "appear" : ""}`} key={element.title}>
                         <div className='gamePicture'>
                             <img className='gamePictureIn' src={element.image} alt={element.title} />
                         </div>
